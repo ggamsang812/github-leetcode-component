ff1279250698dac2c018c666a08e2b8bcecc00aa
ggamsang812 <ggamsang812@gmail.com>
2024-06-15 21:04:33 -0400

    fetch works in dev and storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,9 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
@@ -13,5 +13,17 @@ const config: StorybookConfig = {
     name: "@storybook/react-vite",
     options: {},
   },
+  viteFinal: async (config) => {
+    config.server = config.server || {};
+    config.server.proxy = {
+      "/api": {
+        target: "https://github.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    };
+    return config;
+  },
 };
+
 export default config;
